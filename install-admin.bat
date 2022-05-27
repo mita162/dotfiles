efc65f24e89fb01ba3850b651c7e50a81f204d47
Mita Yuki <mita162@kanazawa-es.com>
2022-05-27 10:43:29 +0900

    add install-admin.bat

---
+++ install-admin.bat
@@ -0,0 +1,10 @@
+rem systems (needs admin)
+winget install Adobe.Acrobat.Reader.64-bit
+winget install ImageMagick
+winget install --silent MPC-HC
+winget install --silent --source msstore VLC
+winget install --silent WinMerge
+winget install --silent DominikReichl.KeePass
+winget install GnuWin32.Make
+winget install GnuWin32.Wget
+winget install --source winget --silent Microsoft.VisualStudio.2019.BuildTools --version 16.11.13
